ba0b19cc8f907983319ed7738b07329bca2eb57c
Guga Coder <guga.coder@gmail.com>
2020-08-27 15:28:03 -0300

    Adição da versão de produção do website

---
+++ client/dist/precache-manifest.4b88b5a65c87154d32c9b563be75e316.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b175ed32df22520d256a",
+    "url": "/css/app.cd672271.css"
+  },
+  {
+    "revision": "960d26200b1b0fe85c08",
+    "url": "/css/chunk-vendors.69192013.css"
+  },
+  {
+    "revision": "66dc854e8e6da281f8beee081d633d70",
+    "url": "/fonts/materialdesignicons-webfont.66dc854e.eot"
+  },
+  {
+    "revision": "8ac434c848d34646f73544f35a903a67",
+    "url": "/fonts/materialdesignicons-webfont.8ac434c8.woff"
+  },
+  {
+    "revision": "8ed8f0719def123d77bf6ea8919b8d8a",
+    "url": "/fonts/materialdesignicons-webfont.8ed8f071.ttf"
+  },
+  {
+    "revision": "c0f7978a13704c522aaf355a80a5d0b0",
+    "url": "/fonts/materialdesignicons-webfont.c0f7978a.woff2"
+  },
+  {
+    "revision": "89c0715060f7494fd60467c549ed250f",
+    "url": "/img/header-400.jpg"
+  },
+  {
+    "revision": "66d03e2e8a4ab2f7b426292e4d42c346",
+    "url": "/img/header.jpg"
+  },
+  {
+    "revision": "adfda8df04c551923e5d7e9265f78f61",
+    "url": "/img/mercadologic-pdv-screenshot.png"
+  },
+  {
+    "revision": "d9b04cebe1434703a55a48cbea232e8d",
+    "url": "/img/supermercado-2.jpg"
+  },
+  {
+    "revision": "66d03e2e8a4ab2f7b426292e4d42c346",
+    "url": "/img/supermercado.jpg"
+  },
+  {
+    "revision": "82499122124de57a8635c8a5348d6840",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b175ed32df22520d256a",
+    "url": "/js/app.12ea5d0b.js"
+  },
+  {
+    "revision": "960d26200b1b0fe85c08",
+    "url": "/js/chunk-vendors.b1fd55ce.js"
+  },
+  {
+    "revision": "87b9a47a9777e8ef60d8",
+    "url": "/js/sandbox.e161d708.js"
+  },
+  {
+    "revision": "b058837529993b1bac5d175fc49aef63",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
