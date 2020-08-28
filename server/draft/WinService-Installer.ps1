82f13fcf0b42bec9199cba05b1300180249a5c58
Guga Coder <guga.coder@gmail.com>
2020-08-28 13:20:19 -0300

    Aplicado uma refatoração na estrutura do projeto para melhor suporte ao PackDm

--- server/draft/WinService-Installer.ps1
+++
@@ -1,9 +0,0 @@
-#$acl = Get-Acl "C:\Program Files (x86)\Processa Sistemas\ProcessaApp"
-#$aclRuleArgs = "Processa.com\guga", "Read,Write,ReadAndExecute", "ContainerInherit,ObjectInherit", "None", "Allow"
-#$accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($aclRuleArgs)
-#$acl.SetAccessRule($accessRule)
-#$acl | Set-Acl "C:\Program Files (x86)\Processa Sistemas\ProcessaApp"
-
-#New-Service -Name ProcessaApp -BinaryPathName "C:\Program Files (x86)\Processa Sistemas\ProcessaApp\Director.exe" -Credential "Processa.com\guga" -Description "Plataforma de aplicativos da Processa." -DisplayName "Processa - App" -StartupType Automatic
-New-Service -Name ProcessaApp -BinaryPathName "C:\Program Files (x86)\Processa Sistemas\ProcessaApp\Director.exe" -Description "Plataforma de aplicativos da Processa." -DisplayName "Processa - App" -StartupType Automatic
-Start-Service -Name ProcessaApp
