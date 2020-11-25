c394c3d5b0253e909b9829188b430acd6589110f
Guga Coder <guga.coder@gmail.com>
2020-11-25 17:35:10 -0300

    Preparacao da construcao do algoritmo de replicacao

---
+++ Server/Mercadologic.Replicacao/Jobs/ReplicacaoJob.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Keep.Paper.Jobs;
+using Keep.Tools;
+
+namespace Mercadologic.Replicacao.Jobs
+{
+  [Expose]
+  public class ReplicacaoJob : IJobAsync
+  {
+    public void SetUp(IJobScheduler scheduler)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task RunAsync(CancellationToken stopToken)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ Server/Mercadologic.Replicacao/Jobs/ReplicacaoPorEmpresaJob.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Keep.Paper.Jobs;
+
+namespace Mercadologic.Replicacao.Jobs
+{
+  public class ReplicacaoPorEmpresaJob : IJobAsync
+  {
+    public void SetUp(IJobScheduler scheduler)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task RunAsync(CancellationToken stopToken)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
